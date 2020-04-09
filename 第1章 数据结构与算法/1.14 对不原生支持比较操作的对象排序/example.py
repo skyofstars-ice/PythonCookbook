5c9db18f8a4d9e1d1f17fc3e14b790b661666b43
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-09 23:16:15 +0800

    更新1.14example

--- 第1章 数据结构与算法/1.14 对不原生支持比较操作的对象排序/example.py
+++ 第1章 数据结构与算法/1.14 对不原生支持比较操作的对象排序/example.py
@@ -1,3 +1,8 @@
+#内建的sorted()函数可接受一个用来传递可调用对象(callable)的参数key，
+#而该可调用对象会返回待排序对象的某些值，sorted则利用这些值来比较对象。
+#例如，如果应用中有一系列的User对象实例，而我们想通过user_id属性来对它们排序，
+#则可以提供一个可调用对象将User实例作为输入然后返回user_id。
+#示例如下：
 class User:
 	def __init__(self, user_id):
 		self.user_id = user_id
