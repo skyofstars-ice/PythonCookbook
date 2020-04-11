5fb9ca0629ed2c0862c4345973dbf8175d09df6a
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-11 22:59:24 +0800

    更新1.16example

--- 第1章 数据结构与算法/1.16 筛选序列中的元素/example.py
+++ 第1章 数据结构与算法/1.16 筛选序列中的元素/example.py
@@ -14,3 +14,19 @@ for i in my_list:
 pos = (n for n in my_list if n > 0)
 for x in pos:
 	print(x)
+
+#有时候筛选的标准没办法简单地表示在列表推导式或者生成器表达式中。
+#比如，假设筛选过程涉及异常处理或者其他一些复杂的细节。
+#基于此，可以将处理筛选逻辑的代码放到单独的函数中，然后使用内建的filter()函数处理。
+values = ['1', '2', '-3', '-', '4', 'N/A', '5']
+
+def is_int(val):
+	try:
+		x = int(val)
+		return True
+	except ValueError:
+		return False
+ivals = list(filter(is_int, values))
+print(ivals)
+#Outputs
+#['1', '2', '-3', '4', '5']
