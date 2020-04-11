30b6f2974786174b57bf3203174809136e4f55d2
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-11 22:53:48 +0800

    更新1.16example

--- 第1章 数据结构与算法/1.16 筛选序列中的元素/example.py
+++ 第1章 数据结构与算法/1.16 筛选序列中的元素/example.py
@@ -7,3 +7,10 @@ for n in my_list:
 for i in my_list:
 	if i < 0:
 		print(i)
+
+#使用列表推导式的一个潜在缺点是如果原始输入非常大的话，这么做可能会产生一个庞大的结果
+#如果考虑这种结果，那么可以用生成器表达式通过迭代的方式产生筛选的结果
+#例如
+pos = (n for n in my_list if n > 0)
+for x in pos:
+	print(x)
