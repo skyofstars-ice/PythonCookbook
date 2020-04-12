9f6d931ca1b9f1ab4981b0d0c18a564c6b525030
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-12 22:57:30 +0800

    更新1.17example

--- 第1章 数据结构与算法/1.17 从字典中提取子集/example.py
+++ 第1章 数据结构与算法/1.17 从字典中提取子集/example.py
@@ -5,7 +5,7 @@ prices = {
 	"HPQ": 37.20,
 	"FB": 10.75
 }
-
+#字典推导式
 #Make a dictionary of all prices over 200
 p1 = { key:value for key, value in prices.items() if value > 200}
 print(p1)
@@ -13,8 +13,19 @@ print(p1)
 #{'AAPL': 612.78, 'IBM': 205.55}
 
 #Make a dictionary of tech stocks
-tech_name = {"AAPL", "IBM", "HPQ", "MSFT"}
-p2 = { key:value for key, value in prices.items() if key in tech_name}
+tech_names = {"AAPL", "IBM", "HPQ", "MSFT"}
+p2 = { key:value for key, value in prices.items() if key in tech_names}
 print(p2)
 #Output
 #{'AAPL': 612.78, 'IBM': 205.55, 'HPQ': 37.2}
+
+#大部分可以用字典推导式解决的问题也可以通过创建元组序列然后将它们传给dict()函数来完成
+#例如
+p3 = dict((key, value) for key, value in prices.items() if value > 200)
+print(p3)
+
+#但是字典推导式的方案更加清晰，而且实际运行起来也要快很多
+#第二个例子还可以重写为
+p4 = { key:prices[key] for key in prices.keys() & tech_names}
+print(p4)
+#但是计时表明这种方案几乎比字典推导式慢上1.6倍
