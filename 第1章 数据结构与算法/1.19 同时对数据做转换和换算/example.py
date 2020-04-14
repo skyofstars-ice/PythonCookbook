690181209cbf79af173eee93cc20a7ce0f674a57
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-14 23:05:30 +0800

    example.py增加跨数据结构字段的数据缩减的例子

--- 第1章 数据结构与算法/1.19 同时对数据做转换和换算/example.py
+++ 第1章 数据结构与算法/1.19 同时对数据做转换和换算/example.py
@@ -15,3 +15,13 @@ print(s)
 #Output a tuple as CSV
 s = ('ACME', 50, 123.45)
 print(','.join(str(x) for x in s))
+
+#Data reduction across fields of a data structure
+portfolio = [
+	{'name': 'GOOG', 'shares': 50},
+	{'name': 'YHOO', 'shares': 75},
+	{'name': 'AOL', 'shares': 20},
+	{'name': 'SCOX', 'shares':65}
+]
+min_shares = min(a['shares'] for a in portfolio)
+print(min_shares)
