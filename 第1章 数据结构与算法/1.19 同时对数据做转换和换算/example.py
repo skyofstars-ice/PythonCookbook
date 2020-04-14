24cf00dc5b1b344be9a83a6b65b3eba7f313de25
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-14 22:59:47 +0800

    example.py增加判断文件夹中是否存在.py文件例子

--- 第1章 数据结构与算法/1.19 同时对数据做转换和换算/example.py
+++ 第1章 数据结构与算法/1.19 同时对数据做转换和换算/example.py
@@ -2,3 +2,12 @@
 nums = [1, 2, 3, 4, 5]
 s = sum(x * x for x in nums)
 print(s)
+
+#Determine if any .py files exist in a directory
+# import os
+# 需要一个path路径
+# files = os.listdir(path)
+# if any(name.endswith('.py') for name in files):
+# 	print('There be Python!')
+# else:
+# 	print('Sorry, no Python.')
