c45b26612026fa6f09a350ab126d6996139da189
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-19 23:32:57 +0800

    更新2.7example

--- 第2章 字符串和文本/2.7 定义实现最短匹配的正则表达式/example.py
+++ 第2章 字符串和文本/2.7 定义实现最短匹配的正则表达式/example.py
@@ -1,6 +1,12 @@
 import re
 str_pat = re.compile(r'\"(.*)\"')
 text1 = 'Computer says "no."'
-print(str_pat.findall(text1))
+print(str_pat.findall(text1)) # ['no.']
+
 text2 = 'Computer says "no." Phone says "yes."'
-print(str_pat.findall(text2))
+print(str_pat.findall(text2)) # ['no." Phone says "yes.']
+
+#  *操作符是贪心策略，所以匹配过程中是基于找出最长的可能匹配来进行的。
+#  要解决这个在text2中错误匹配的问题，只要在模式中的*操作符后加上？修饰符即可
+new_str_pat = re.compile(r'\"(.*?)\"')
+print(new_str_pat.findall(text2)) # ['no.', 'yes.']
