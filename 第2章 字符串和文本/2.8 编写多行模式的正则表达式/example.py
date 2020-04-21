2c0a56a9eb2637868d4cb9e3bca633eed69b9926
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-21 23:46:06 +0800

    创建2.8example

---
+++ 第2章 字符串和文本/2.8 编写多行模式的正则表达式/example.py
@@ -0,0 +1,12 @@
+import re
+comment = re.compile(r'/\*(.*?)\*/')
+text1 = '/*this is a comment*/'
+text2 = '''/*this is a
+				multiline comment*/
+	
+'''
+
+a = comment.findall(text1)
+print(a)
+b = comment.findall(text2)
+print(b)
