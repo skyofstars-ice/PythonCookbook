d40cda34c38f9c904064b3f64323504e7683c0bd
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-22 00:37:31 +0800

    修改

--- 第2章 字符串和文本/2.8 编写多行模式的正则表达式/example.py
+++ 第2章 字符串和文本/2.8 编写多行模式的正则表达式/example.py
@@ -15,6 +15,3 @@ comment_text = re.compile(r'/\*((?:.|\n)*?)\*/')
 c = comment_text.findall(text2)
 print(c)
 
-comment_text_2 = re.compile(r'/\*(.*?)\*/', re.DOTALL)
-d = comment_text_2.findall(text2)
-print(d)
