dac8f83ad2093aa55c2212a4c4aa6139f899fe89
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-26 21:26:53 +0800

    更新2.13example3

--- 第2章 字符串和文本/2.13 对齐文本字符串/example3.py
+++ 第2章 字符串和文本/2.13 对齐文本字符串/example3.py
@@ -1,9 +1,15 @@
 text = 'Hello World'
-a = format(text,'>20')
+a = format(text, '>20')
 print(a)  #  '         Hello World'
 
-b = format(text,'<20')
+b = format(text, '<20')
 print(b)  #  'Hello World         '
 
-c = format(text,'^20')
+c = format(text, '^20')
 print(c)  #  '    Hello World     '
+
+d = format(text, '=>20s')
+print(d)  #'=========Hello World'
+
+e = format(text, '*^20s')
+print(e)  #'****Hello World*****'
