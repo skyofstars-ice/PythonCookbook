e18e72ad60a19d52e46e48d6bf330731f927971a
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-27 23:38:12 +0800

    更新2.14example 增加生成器表达式操作

--- 第2章 字符串和文本/2.14 字符串连接及合并/example.py
+++ 第2章 字符串和文本/2.14 字符串连接及合并/example.py
@@ -14,3 +14,9 @@ parttwo = 'Not Chicago?'
 
 d = '{} {}'.format(partone,parttwo)
 print(d)  #  Is Chicago Not Chicago?
+
+
+# 利用生成器表达式将数据转换为字符串的同时完成连接操作
+data = ['ACME', 50, 91.1]
+e = ','.join(str(d) for d in data)
+print(e)  #  ACME,50,91.1
