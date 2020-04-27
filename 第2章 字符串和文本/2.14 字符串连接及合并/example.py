3232e0275791d21354924988d6674f672369f8b2
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-27 23:34:57 +0800

    更新2.14example

--- 第2章 字符串和文本/2.14 字符串连接及合并/example.py
+++ 第2章 字符串和文本/2.14 字符串连接及合并/example.py
@@ -8,3 +8,9 @@ print(b)  #  Is,Chicago,Not,Chicago?
 
 c = ''.join(parts)
 print(c)  #  IsChicagoNotChicago?
+
+partone = 'Is Chicago'
+parttwo = 'Not Chicago?'
+
+d = '{} {}'.format(partone,parttwo)
+print(d)  #  Is Chicago Not Chicago?
