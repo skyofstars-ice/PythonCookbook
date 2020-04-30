3a40e62497f0b0698c18fb170f2ff5e215ce714f
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-30 22:59:28 +0800

    更新2.16example

--- 第2章 字符串和文本/2.16 以固定的列数重新格式化文本/example.py
+++ 第2章 字符串和文本/2.16 以固定的列数重新格式化文本/example.py
@@ -5,3 +5,12 @@ look into my eyes, you're under."
 import textwrap
 
 print(textwrap.fill(s, 70))
+# Look into my eyes, look into my eyes, the eyes, the eyes, the eyes,
+# not around the eyes, don't look around the eyes, look into my eyes,
+# you're under.
+
+print(textwrap.fill(s, 40))
+# Look into my eyes, look into my eyes,
+# the eyes, the eyes, the eyes, not around
+# the eyes, don't look around the eyes,
+# look into my eyes, you're under.
