0ec4626d70eccad5058408907be16fddd78710f5
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-04-30 23:03:19 +0800

    更新2.16example

--- 第2章 字符串和文本/2.16 以固定的列数重新格式化文本/example.py
+++ 第2章 字符串和文本/2.16 以固定的列数重新格式化文本/example.py
@@ -14,3 +14,15 @@ print(textwrap.fill(s, 40))
 # the eyes, the eyes, the eyes, not around
 # the eyes, don't look around the eyes,
 # look into my eyes, you're under.
+
+print(textwrap.fill(s, 40, initial_indent=' '))
+#  Look into my eyes, look into my eyes,
+# the eyes, the eyes, the eyes, not around
+# the eyes, don't look around the eyes,
+# look into my eyes, you're under.
+
+print(textwrap.fill(s, 40, subsequent_indent=' '))
+# Look into my eyes, look into my eyes,
+#  the eyes, the eyes, the eyes, not
+#  around the eyes, don't look around the
+#  eyes, look into my eyes, you're under.
