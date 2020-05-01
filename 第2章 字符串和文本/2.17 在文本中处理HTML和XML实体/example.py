4a33c43b6c78539130c7598ccc225a4fe45427ec
skyofstars-ice <skyofstars-ice@users.noreply.github.com>
2020-05-01 21:42:12 +0800

    更新2.17example

--- 第2章 字符串和文本/2.17 在文本中处理HTML和XML实体/example.py
+++ 第2章 字符串和文本/2.17 在文本中处理HTML和XML实体/example.py
@@ -1,3 +1,5 @@
 import html
 s = 'Elements are written as "<tag>text</tag>".'
 print(s)  #  Elements are written as "<tag>text</tag>".
+
+print(html.escape(s)) # Elements are written as &quot;&lt;tag&gt;text&lt;/tag&gt;&quot;.
